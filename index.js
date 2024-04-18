cf239ff44e443795afee8a050d8d1ce64ac9e46a
Vagner Bom Jesus <48354097+VagnerBomJesus@users.noreply.github.com>
2024-04-18 10:57:33 +0000

    add vercel

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const nodemailer = require('nodemailer');
 const createError = require("http-errors");
 const PORT = process.env.PORT || 8080;
 
@@ -6,10 +7,43 @@ const app = express();
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
+app.use(express.urlencoded({ extended: true })); // Para parsing application/x-www-form-urlencoded
+app.use(express.json()); // Para parsing application/json
+
+// Configuração do Nodemailer
+const transporter = nodemailer.createTransport({
+  service: 'Gmail', // Use outros como Outlook, Yahoo, etc., ou SMTP direto
+  auth: {
+    user: 'vagneripg@gmail.com',
+    pass: 'sua-senha'
+  }
+});
 
 app.get("/", (req, res) => {
   res.render("index.ejs", {});
 });
+
+
+app.post('/send-email', (req, res) => {
+  const { name, email, message } = req.body;
+  const mailOptions = {
+    from: 'vagneripg@gmail.com',
+    to: 'vagneripg@gmail.com',
+    subject: `Mensagem de ${name}`,
+    text: `Você recebeu uma mensagem de ${name} (${email}): ${message}`
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+      res.send('error'); // ou renderizar uma página de erro
+    } else {
+      console.log('Email enviado: ' + info.response);
+      res.send('sent'); // ou renderizar uma página de sucesso
+    }
+  });
+});
+
 
 app.get('/cv', (req, res) => {
   res.render('cv', {});
