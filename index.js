ac9035d2ffc718c44608aa9c3f04887c91f54298
Vagner Bom Jesus <48354097+VagnerBomJesus@users.noreply.github.com>
2024-04-19 13:29:49 +0000

    update interface web

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-  // Assegure-se de instalar com npm install dotenv
+// Assegure-se de instalar com npm install dotenv
 const express = require("express");
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 const createError = require("http-errors");
-const userRoutes = require('./routes/userRoutes.js');
-const blogRoutes = require('./routes/blogRoutes.js');
+const userRoutes = require("./routes/userRoutes.js");
+const blogRoutes = require("./routes/blogRoutes.js");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
@@ -15,51 +15,74 @@ app.use(express.json());
 
 // Configuração do Nodemailer
 const transporter = nodemailer.createTransport({
-  service: 'Gmail',
+  service: "Gmail",
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD
-  }
+    pass: process.env.EMAIL_PASSWORD,
+  },
 });
 
 app.get("/", (req, res) => {
   res.render("index.ejs", {});
 });
 
-app.post('/send-email', (req, res) => {
+app.post("/send-email", (req, res) => {
   const { name, email, message } = req.body;
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to: process.env.EMAIL_USER,
     subject: `Mensagem de ${name}`,
-    text: `Você recebeu uma mensagem de ${name} (${email}): ${message}`
+    text: `Você recebeu uma mensagem de ${name} (${email}): ${message}`,
   };
 
-  transporter.sendMail(mailOptions, function(error, info){
+  transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log(error);
-      res.send('error');
+      res.send("error");
     } else {
-      console.log('Email enviado: ' + info.response);
-      res.send('sent');
+      console.log("Email enviado: " + info.response);
+      res.send("sent");
     }
   });
 });
 
-app.get('/cv', (req, res) => {
-  res.render('cv', {});
+app.get("/cv", (req, res) => {
+  res.render("cv", {});
 });
 
-app.use('/users', userRoutes);
-app.use('/blogs', blogRoutes);
+app.use("/users", userRoutes);
+app.use("/blogs", blogRoutes);
+// Rota para a página de Login
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
+// Rota para a página de Registro
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+// Rota para a criação de Blog
+app.get("/create-blog", (req, res) => {
+  res.render("create_blog");
+});
+
+// Rota para adicionar Comentários em um Blog específico
+app.get("/add-comment/:blogId", (req, res) => {
+  const blogId = req.params.blogId;
+  res.render("add_comment", { blogId: blogId });
+});
+
+app.get("/cv", (req, res) => {
+  res.render("cv", {});
+});
 app.use((req, res, next) => {
   next(createError(404, "Página Não Encontrada"));
 });
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
-  res.render("404.ejs", {error: err});
+  res.render("404.ejs", { error: err });
 });
 
 app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
